aa250999c10988fc8fb7cb0e8f2282703efa1399
key-er <nasaquba@gmail.com>
2018-08-20 16:08:48 -0700

    moved fetch of service registry data from client to server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
-// var items = require('../database-mysql');
-// var items = require('../database-mongo');
-
-
+var request = require('request')
 var app = express();
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -18,15 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
   API Routes
  + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +*/
 
-app.get('/', (req,res)=> {
-  res.sendFile('index.html', { root: 'client/public'});
+app.get('/monitor', (req,res)=> {
+  request('http://localhost:9001/getMicroservices', (error, response, body) => {
+    if (error) res.send('Error while getting microservices')
+    else res.send(body)
+  })
+
+  // res.sendFile('client/dist/index.html');
 });
-app.get('/items/:id', getItems);
-app.get('/products', getProducts);
-
-app.post('/login', login);
-app.post('/signup', signup);
-
 
 /* + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +
   API Route Functions
@@ -57,7 +51,7 @@ function signup() {
 
 }
 
-var port = process.env.PORT || 3200;
+var port = process.env.PORT || 9000;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);

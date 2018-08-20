fb856f7daddb7981d1abccdaee735d92f4ae28b9
key-er <nasaquba@gmail.com>
2018-08-20 16:16:36 -0700

    removed forefox developer from karma due to error

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [ 'Chrome' , 'Firefox', 'FirefoxDeveloper'],
+    browsers: [ 'Chrome' , 'Firefox'],
 
 
     // Continuous Integration mode
